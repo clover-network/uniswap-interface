ae395a1fc0fbfc830a525bdd162692b8da99580d
Taneeya <2240351540@qq.com>
2022-11-04 15:11:13 +0800

    change WETH to WCLV

--- src/hooks/useWrapCallback.ts
+++ src/hooks/useWrapCallback.ts
@@ -48,13 +48,13 @@ export default function useWrapCallback(
             ? async () => {
                 try {
                   const txReceipt = await wethContract.deposit({ value: `0x${inputAmount.quotient.toString(16)}` })
-                  addTransaction(txReceipt, { summary: `Wrap ${inputAmount.toSignificant(6)} ETH to WETH` })
+                  addTransaction(txReceipt, { summary: `Wrap ${inputAmount.toSignificant(6)} CLV to WCLV` })
                 } catch (error) {
                   console.error('Could not deposit', error)
                 }
               }
             : undefined,
-        inputError: sufficientBalance ? undefined : hasInputAmount ? 'Insufficient ETH balance' : 'Enter ETH amount',
+        inputError: sufficientBalance ? undefined : hasInputAmount ? 'Insufficient CLV balance' : 'Enter CLV amount',
       }
     } else if (weth.equals(inputCurrency) && outputCurrency.isNative) {
       return {
@@ -64,13 +64,13 @@ export default function useWrapCallback(
             ? async () => {
                 try {
                   const txReceipt = await wethContract.withdraw(`0x${inputAmount.quotient.toString(16)}`)
-                  addTransaction(txReceipt, { summary: `Unwrap ${inputAmount.toSignificant(6)} WETH to ETH` })
+                  addTransaction(txReceipt, { summary: `Unwrap ${inputAmount.toSignificant(6)} WCLV to CLV` })
                 } catch (error) {
                   console.error('Could not withdraw', error)
                 }
               }
             : undefined,
-        inputError: sufficientBalance ? undefined : hasInputAmount ? 'Insufficient WETH balance' : 'Enter WETH amount',
+        inputError: sufficientBalance ? undefined : hasInputAmount ? 'Insufficient WCLV balance' : 'Enter WCLV amount',
       }
     } else {
       return NOT_APPLICABLE
