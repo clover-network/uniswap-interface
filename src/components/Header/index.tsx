f965fb842be3086f75a729b3f7ef64d34021cc3d
werlandy <422606101@qq.com>
2022-11-09 16:00:02 +0800

    fix balance issue

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -306,7 +306,7 @@ export default function Header() {
           <AccountElement active={!!account}>
             {account && userEthBalance ? (
               <BalanceText style={{ flexShrink: 0, userSelect: 'none' }} pl="0.75rem" pr="0.5rem" fontWeight={500}>
-                <Trans>{userEthBalance?.toSignificant(3)} CLV</Trans>
+                {userEthBalance?.toSignificant(3)} CLV
               </BalanceText>
             ) : null}
             <Web3Status />
